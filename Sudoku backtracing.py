3de3bcb979013bdf3abc8247d65b7ca4453dd492
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-10-26 15:19:17 +0300

    Add files via upload

---
+++ Sudoku backtracing.py
@@ -0,0 +1,21 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def solve(row, col):
+    if col == board[row].length:
+
+
+    for i in range(1,10):
+        board[row][col]= i
+        if(ValidPlacement(row,col)): 
+            if(solve(row,col+1,board)):
+                return True;
